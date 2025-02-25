e8a158d81d15230b062f555813ed3a940fe318fd
badrAG <badrag6@gmail.com>
2025-02-25 01:31:13 +0000

    upgrade

--- src/queries/sql/events/getEventDataStats.ts
+++ src/queries/sql/events/getEventDataStats.ts
@@ -1,19 +1,19 @@
-import prisma from 'lib/prisma';
-import clickhouse from 'lib/clickhouse';
-import { CLICKHOUSE, PRISMA, runQuery } from 'lib/db';
-import { QueryFilters } from 'lib/types';
+import prisma from '@/lib/prisma';
+import clickhouse from '@/lib/clickhouse';
+import { CLICKHOUSE, PRISMA, runQuery } from '@/lib/db';
+import { QueryFilters } from '@/lib/types';
 
 export async function getEventDataStats(
   ...args: [websiteId: string, filters: QueryFilters]
 ): Promise<{
   events: number;
-  fields: number;
+  properties: number;
   records: number;
 }> {
   return runQuery({
     [PRISMA]: () => relationalQuery(...args),
     [CLICKHOUSE]: () => clickhouseQuery(...args),
-  }).then(results => results[0]);
+  }).then(results => results?.[0]);
 }
 
 async function relationalQuery(websiteId: string, filters: QueryFilters) {
@@ -24,18 +24,18 @@ async function relationalQuery(websiteId: string, filters: QueryFilters) {
     `
     select 
       count(distinct t.website_event_id) as "events",
-      count(distinct t.event_key) as "fields",
+      count(distinct t.data_key) as "properties",
       sum(t.total) as "records"
     from (
       select
         website_event_id,
-        event_key,
+        data_key,
         count(*) as "total"
       from event_data
       where website_id = {{websiteId::uuid}}
         and created_at between {{startDate}} and {{endDate}}
       ${filterQuery}
-      group by website_event_id, event_key
+      group by website_event_id, data_key
       ) as t
     `,
     params,
@@ -45,7 +45,7 @@ async function relationalQuery(websiteId: string, filters: QueryFilters) {
 async function clickhouseQuery(
   websiteId: string,
   filters: QueryFilters,
-): Promise<{ events: number; fields: number; records: number }[]> {
+): Promise<{ events: number; properties: number; records: number }[]> {
   const { rawQuery, parseFilters } = clickhouse;
   const { filterQuery, params } = await parseFilters(websiteId, filters);
 
@@ -53,28 +53,20 @@ async function clickhouseQuery(
     `
     select 
       count(distinct t.event_id) as "events",
-      count(distinct t.event_key) as "fields",
+      count(distinct t.data_key) as "properties",
       sum(t.total) as "records"
     from (
       select
         event_id,
-        event_key,
+        data_key,
         count(*) as "total"
       from event_data
       where website_id = {websiteId:UUID}
         and created_at between {startDate:DateTime64} and {endDate:DateTime64}
       ${filterQuery}
-      group by event_id, event_key
+      group by event_id, data_key
       ) as t
     `,
     params,
-  ).then(a => {
-    return Object.values(a).map(a => {
-      return {
-        events: Number(a.events),
-        fields: Number(a.fields),
-        records: Number(a.records),
-      };
-    });
-  });
+  );
 }
